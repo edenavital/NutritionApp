d0782b80f6fefc2ba20098bbe11b83b9ab58e369
edenavital <edendor@gmail.com>
2020-04-15 00:37:22 +0300

    Modified the usage of tokens - created auth.js middleware, added token to reducer of user, aded default.json - config file which needs to be modified for production too, modified server.js so it will use auth middleware

--- frontend/src/redux/user/userReducer.js
+++ frontend/src/redux/user/userReducer.js
@@ -1,7 +1,9 @@
 import { SAVE_DATA_FROM_DATABASE, RESET_STATE_APP } from "./userTypes";
 
 const initialState = {
-  data: [{}],
+  credentials: null,
+  food: null,
+  token: localStorage.getItem("JWT"),
 };
 
 const userReducer = (state = initialState, action) => {
@@ -10,10 +12,12 @@ const userReducer = (state = initialState, action) => {
       return {
         credentials: action.payload.credentials,
         food: action.payload.food,
+        token: action.payload.token,
       };
     case RESET_STATE_APP:
       return {
         ...initialState,
+        token: null,
       };
     default:
       return state;
