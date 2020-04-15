69496289bd2d5c3a52dd168588186782aad86ed2
Ali-mszd <ali.moosazadeh92@gmail.com>
2020-04-15 19:02:55 +0430

    test develop

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
 const auth = require("./middleware/auth");
-//Great, ali is pushing for the first time
+//test only on develop;
 //Config for working with postgres in localhost environment: (comes from default.json file now - environment json)
 
 let configPg = config.get("devConfig");
