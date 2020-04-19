7fcaabcaa108f3e7fa5ac46fb1e753cc99f499ef
Ali-mszd <ali.moosazadeh92@gmail.com>
2020-04-19 11:17:27 +0430

    Login page responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,20 +8,23 @@ import Food from "./components/Food/Food";
 import AuthenticationComp from "./containers/AuthenticationComp/AuthenticationComp";
 import { ROUTERPATHS } from "./constants/constants";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import { BreakpointProvider } from "react-socks";
 
 const App = () => {
   return (
     <div className="App">
       <NotificationContainer />
-      <Router>
-        <Switch>
-          <ProtectedRoute exact path={ROUTERPATHS.FOOD} component={Food} />
-          <ProtectedRoute exact path={ROUTERPATHS.HOME} component={Home} />
-          <Route path={ROUTERPATHS.REGISTER} component={Register} />
-          <Route path={ROUTERPATHS.LOGIN} component={Login} />
-          <Route path={ROUTERPATHS.ROOT} component={AuthenticationComp} />
-        </Switch>
-      </Router>
+      <BreakpointProvider>
+        <Router>
+          <Switch>
+            <ProtectedRoute exact path={ROUTERPATHS.FOOD} component={Food} />
+            <ProtectedRoute exact path={ROUTERPATHS.HOME} component={Home} />
+            <Route path={ROUTERPATHS.REGISTER} component={Register} />
+            <Route path={ROUTERPATHS.LOGIN} component={Login} />
+            <Route path={ROUTERPATHS.ROOT} component={AuthenticationComp} />
+          </Switch>
+        </Router>
+      </BreakpointProvider>
     </div>
   );
 };
--- frontend/src/containers/Login/Login.js
+++ frontend/src/containers/Login/Login.js
@@ -5,6 +5,8 @@ import Input from "../../components/Input/Input";
 import Label from "../../components/Label/Label";
 import Button from "../../components/Button/Button";
 import { Link } from "react-router-dom";
+import { Breakpoint } from "react-socks";
+import { motion } from "framer-motion";
 import axios from "axios";
 import {
   saveDataLogin,
@@ -22,7 +24,6 @@ class Login extends Component {
       password: "",
     },
   };
-
   handleInputChange = (e) => {
     const key = e.target.id;
     const value = e.target.value;
@@ -52,54 +53,126 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="Login">
-          <Icon iconName="diet" width="100px" height="100px" />
-          <div className="description pt-4">
-            <h4 className="text-success">Welcome to Nutrition App</h4>
-            <p>Sign in to continue</p>
+      <>
+        <Breakpoint large down>
+          <div className="d-flex min-vh-100 bg-white justify-content-center align-items-center">
+            <div className="col-md-6 col-lg-7">
+              <div className="card-body">
+                <Icon iconName="diet" width="100px" height="100px" />
+                <div className="description pt-4">
+                  <h4 className="text-success">Welcome to Nutrition App</h4>
+                  <p>Sign in to continue</p>
+                </div>
+
+                <form onSubmit={this.onSubmitForm}>
+                  <div className="float-input-box pt-1">
+                    <input
+                      className={
+                        this.state.person.username.length !== 0
+                          ? "input-filled"
+                          : ""
+                      }
+                      type="text"
+                      id="username"
+                      value={this.state.person.username}
+                      onChange={this.handleInputChange}
+                      required
+                    />
+                    <label htmlFor="username">Username</label>
+                  </div>
+                  <div className="float-input-box">
+                    <input
+                      className={
+                        this.state.person.password.length !== 0
+                          ? "input-filled"
+                          : ""
+                      }
+                      type="password"
+                      id="password"
+                      value={this.state.person.password}
+                      onChange={this.handleInputChange}
+                      required
+                    />
+                    <label htmlFor="password">Password</label>
+                  </div>
+                  <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    className="btn btn-success text-left mb-3 mt-2"
+                    type="submit"
+                  >
+                    Sign in
+                  </motion.button>
+                </form>
+
+                <p className="Signin">
+                  <Link to="/register">Sign up</Link> For an account
+                </p>
+              </div>
+            </div>
           </div>
+        </Breakpoint>
+        <Breakpoint xlarge up>
+          <div className="container">
+            <div className="d-flex min-vh-100 justify-content-center align-items-center">
+              <div className="col-5">
+                <div className="p-4 bg-white shadow rounded-lg">
+                  <Icon iconName="diet" width="100px" height="100px" />
+                  <div className="description pt-4">
+                    <h4 className="text-success">Welcome to Nutrition App</h4>
+                    <p>Sign in to continue</p>
+                  </div>
 
-          <form onSubmit={this.onSubmitForm}>
-            <div className="float-input-box pt-1">
-              <input
-                className={
-                  this.state.person.username.length !== 0 ? "input-filled" : ""
-                }
-                type="text"
-                id="username"
-                value={this.state.person.username}
-                onChange={this.handleInputChange}
-                required
-              />
-              <label htmlFor="username">Username</label>
-            </div>
-            <div className="float-input-box">
-              <input
-                className={
-                  this.state.person.username.length !== 0 ? "input-filled" : ""
-                }
-                type="password"
-                id="password"
-                value={this.state.person.password}
-                onChange={this.handleInputChange}
-                required
-              />
-              <label htmlFor="password">Password</label>
-            </div>
-            <Button
-              type="submit"
-              dynamicstyle={{ marginTop: "30px", marginBottom: "15px" }}
-            >
-              SIGN IN
-            </Button>
-          </form>
+                  <form onSubmit={this.onSubmitForm}>
+                    <div className="float-input-box pt-1">
+                      <input
+                        className={
+                          this.state.person.username.length !== 0
+                            ? "input-filled"
+                            : ""
+                        }
+                        type="text"
+                        id="username"
+                        value={this.state.person.username}
+                        onChange={this.handleInputChange}
+                        required
+                      />
+                      <label htmlFor="username">Username</label>
+                    </div>
+                    <div className="float-input-box">
+                      <input
+                        className={
+                          this.state.person.password.length !== 0
+                            ? "input-filled"
+                            : ""
+                        }
+                        type="password"
+                        id="password"
+                        value={this.state.person.password}
+                        onChange={this.handleInputChange}
+                        required
+                      />
+                      <label htmlFor="password">Password</label>
+                    </div>
+                    <motion.button
+                      whileHover={{ scale: 1.1 }}
+                      whileTap={{ scale: 0.9 }}
+                      className="btn btn-success text-left mb-3 mt-2"
+                      type="submit"
+                    >
+                      Sign in
+                    </motion.button>
+                  </form>
 
-          <p className="Signin">
-            <Link to="/register">SIGN UP</Link> FOR AN ACCOUNT
-          </p>
-        </div>
-      </div>
+                  <p className="Signin">
+                    <Link to="/register">Sign up</Link> For an account
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Breakpoint>
+      </>
     );
   }
 }
