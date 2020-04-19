b82804e119a9adc27e576400eeae876fa4482caa
Ali-mszd <ali.moosazadeh92@gmail.com>
2020-04-19 22:20:58 +0430

    notification bug fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,6 @@ import * as serviceWorker from "./serviceWorker";
 
 import { Provider } from "react-redux";
 import { NotificationContainer } from "react-notifications";
-
 import store from "./redux/store";
 
 const app = (
